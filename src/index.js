64e384c0ab1fb7179c3d7218fe1add89864837a0
Raman Hira <ramanhira@gmail.com>
2023-04-08 21:01:33 +0530

    corrected the rotuer path

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/*" element={<App />}/>
+        <Route path="/router-6/*" element={<App />}/>
       </Routes>
     </Router>
   </React.StrictMode>
